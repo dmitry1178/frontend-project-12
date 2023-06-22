f97969ea09f3957224680850543e4587836fb775
Dmitry Nesterov <media.dn11@gmail.com>
2023-06-22 17:06:46 +0300

    start

--- frontend/src/i18n/resources/index.js
+++
@@ -1,5 +0,0 @@
-import ru from './ru.js';
-import en from './en.js';
-
-const languages = { ru, en };
-export default languages;
--- frontend/src/pages/toasts/index.js
+++
@@ -1,8 +0,0 @@
-import { toast } from 'react-toastify';
-
-const toastSuccess = (text) => toast.success(text);
-export const toastInfo = (text) => toast.info(text);
-export const toastWarning = (text) => toast.warn(text);
-export const toastError = (text) => toast.error(text);
-
-export default toastSuccess;
